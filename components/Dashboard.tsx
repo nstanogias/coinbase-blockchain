f33aff4a3825d8d6faa3660bb6ed4d1581bdf6b7
nstanogias <nstanogias@gmail.com>
2022-06-11 13:41:57 +0300

    correct id

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -50,7 +50,7 @@ const Dashboard = ({ address }) => {
       const sdk = new ThirdwebSDK(
         new ethers.Wallet(
           process.env.NEXT_PUBLIC_METAMASK_KEY!,
-          ethers.getDefaultProvider(`https://rinkeby.infura.io/v3/${process.env.NEXT_PUBLIC_METAMASK_KEY!}`)
+          ethers.getDefaultProvider(`https://rinkeby.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_PROJECT_ID!}`)
         )
       );
 
